c35399fa3611036232928485790333af804349f5
Dani Ronda Palasi <48550350+jefatura2000@users.noreply.github.com>
2021-04-24 08:58:45 +0200

    Primer commit

--- www/js/init.js
+++ www/js/init.js
@@ -3,7 +3,26 @@
 
     $('.sidenav').sidenav();
     $('.tabs').tabs({"swipeable":true});
+    $('#buscar').click(function(){
+      $("#colect").empty();
+      $.ajax({
+        method: "GET",
+        url: "https://musicbrainz.org/ws/2/artist?query="+$("#input").val(),
+        dataType: "json",   // necessitem això pq ens retorni un objecte JSON
+      }).done(function (msg) {
+        for(var item in msg.artists) {
+          console.log(msg.artists[item]);
+        
+          $("#colect").append("<li class='collection-item'><div>"+msg.artists[item].name+"   "+msg.artists[item].disambiguation+"<a href='#!' class='secondary-content'><i class='material-icons'>send</i></a></div></li>");
+          
 
+        };
+      }).fail(function () {
+        alert("ERROR");
+      });
+
+    });
+    
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
